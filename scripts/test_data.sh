a8c20cd1ea7c195dce7767faaed253ba562779fd
Nikita Borisov <n.borisov@innopolis.university>
2024-07-06 15:57:48 +0300

    Added data.csv to DVC

--- scripts/test_data.sh
+++ scripts/test_data.sh
@@ -2,12 +2,12 @@
 python src/data.py
 # version the data
 dvc add data/samples/sample.csv
-git add data/samples/sample.csv
+git add data/samples/sample.csv.dvc
 git commit -m "Added data.csv to DVC"
 git push origin master:dev
 # Read version from data.data_version.yaml
 version=$(cat data.data_version.yaml | grep 'version' | cut -d ' ' -f 2)
 
 git tag v$version
-git push origin dev v$version
+git push origin master:dev v$version
 dvc push

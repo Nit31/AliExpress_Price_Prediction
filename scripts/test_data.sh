dcf50edbc6e6100a8939e696eb7cb07512831fda
Nit31 <dydalinnnov31@gmail.com>
2024-07-10 00:42:24 +0300

    Added dvc to data_extract_dag

--- scripts/test_data.sh
+++ scripts/test_data.sh
@@ -6,7 +6,7 @@ git add data/samples/sample.csv.dvc
 git commit -m "Added data.csv to DVC"
 git push origin master:dev
 # Read version from data.data_version.yaml
-version=$(cat data.data_version.yaml | grep 'version' | cut -d ' ' -f 2)
+version=$(cat configs/data_version.yaml | grep 'version' | cut -d ' ' -f 2)
 
 git tag v$version
 git push origin master:dev v$version
--- src/data.py
+++ src/data.py
@@ -1,9 +1,8 @@
 import os
 from datetime import datetime
-
 import pandas as pd
 import hydra
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import DictConfig
 import yaml
 import great_expectations as gx
 from great_expectations.data_context import FileDataContext
@@ -13,12 +12,10 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from transformers import RobertaTokenizer, RobertaModel
 import torch
-from torch.utils.data import DataLoader, TensorDataset, Dataset
-import torch.nn as nn
-import torch.nn.functional as F
-from torch import optim
+from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
 import zenml
+
 
 class TextDataset(Dataset):
     def __init__(self, text_series):
@@ -29,6 +26,7 @@ class TextDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.text_series.iloc[idx]
+
 
 def collate_fn(batch):
     tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
@@ -69,61 +67,66 @@ def sample_data(cfg):
     with open("configs/confidential.yaml", "r") as stream:
         config_confidential_data = yaml.safe_load(stream)
 
-    kaggle_data = config_confidential_data['kaggle']
+    # Try to open the local copy of raw data
+    try:
+        df = pd.read_csv(cfg.db.data_path)
+        print('Dataset local cache is detected.')
+    # In case of failure, download the raw data from kaggle
+    except Exception:
+        print('Dataset local cache not detected. Downloading...')
+        kaggle_data = config_confidential_data['kaggle']
 
-    os.environ['KAGGLE_USERNAME'] = kaggle_data['kaggle_username']
-    os.environ['KAGGLE_KEY'] = kaggle_data['kaggle_key']
+        os.environ['KAGGLE_USERNAME'] = kaggle_data['kaggle_username']
+        os.environ['KAGGLE_KEY'] = kaggle_data['kaggle_key']
 
-    # We need to import API library after setting up environmental variables
-    # if the order will be changed, API will not work properly
-    from kaggle.api.kaggle_api_extended import KaggleApi
+        # We need to import API library after setting up environmental variables
+        # if the order will be changed, API will not work properly
+        from kaggle.api.kaggle_api_extended import KaggleApi
 
-    def download_kaggle_dataset(dataset_name, output_dir):
-        """Function to download Kaggle dataset using API
-        Args:
-            dataset_name (_type_): _description_
-            output_dir (_type_): _description_
-        """
-        api = KaggleApi()
-        api.authenticate()  # Make sure to set up your Kaggle API authentication as mentioned in the previous messages
+        def download_kaggle_dataset(dataset_name, output_dir):
+            """Function to download Kaggle dataset using API
+            Args:
+                dataset_name (_type_): _description_
+                output_dir (_type_): _description_
+            """
+            api = KaggleApi()
+            api.authenticate()  # Make sure to set up your Kaggle API authentication as mentioned in the previous messages
 
-        try:
-            api.dataset_download_files(dataset_name, path=output_dir, unzip=True)
-            print("Dataset downloaded successfully to:", output_dir)
-        except Exception as e:
-            print("Error downloading dataset:", str(e))
+            try:
+                api.dataset_download_files(dataset_name, path=output_dir, unzip=True)
+                print("Dataset downloaded successfully to:", output_dir)
+            except Exception as e:
+                print("Error downloading dataset:", str(e))
 
-    dataset_name = f'{cfg.db.db_creator}/{cfg.db.db_name}'  # Replace with the actual Kaggle dataset name
-    output_directory = "."  # Specify where you want to download the dataset__
+        dataset_name = f'{cfg.db.db_creator}/{cfg.db.db_name}'  # Replace with the actual Kaggle dataset name
+        output_directory = "."  # Specify where you want to download the dataset__
 
-    download_kaggle_dataset(dataset_name, output_directory)
+        download_kaggle_dataset(dataset_name, output_directory)
 
-    # Convert downloaded CSV to Pandas DataFrame
-    df = pd.read_csv(f'{cfg.db.kaggle_filename}_csv.csv')
+        # Convert downloaded CSV to Pandas DataFrame
+        df = pd.read_csv(f'{cfg.db.kaggle_filename}_csv.csv')
 
-    # Remove temporary file
-    os.remove(f'{cfg.db.kaggle_filename}_csv.csv')
-    os.remove(f'{cfg.db.kaggle_filename}_json.json')
-    
-    # Number of sample part that we need(can be changed in config/main.yaml)
-    number_of_sample = cfg.db.sample_part
+        # Save the raw dataset
+        df.to_csv(cfg.db.data_path)
+
+        # Remove temporary file
+        os.remove(f'{cfg.db.kaggle_filename}_csv.csv')
+        os.remove(f'{cfg.db.kaggle_filename}_json.json')
+        
     df_sortes = df.sort_values(by=['lunchTime'])
-    if number_of_sample not in [1, 2, 3, 4, 5]:
-        print('Number of the sample should be < that 6 and > 0')
+    if not 1 <= cfg.db.sample_part <= 5:
+        print('sample_part should be < that 6 and > 0')
         exit(0)
-    if number_of_sample not in [1, 2, 3, 4, 5]:
-        print('Number of the sample should be < that 6 and > 0')
-        exit(0)
-    df_sample = df_sortes[(len(df_sortes) // 5) * (number_of_sample - 1):(len(df_sortes) // 5) * (number_of_sample)]
-    df_sample.to_csv(cfg.db.sample_path)
+    df_sample = df_sortes[(len(df_sortes) // 5) * (cfg.db.sample_part - 1):(len(df_sortes) // 5) * (cfg.db.sample_part)]
+    # df_sample.to_csv(cfg.db.sample_path)
+    return df_sample
 
 
-def handle_initial_data(cfg):
+def handle_initial_data(cfg, sample):
     """
     This function cleans the raw data.
     """
-
-    df = pd.read_csv(cfg.db.sample_path)
+    df = sample
     df = df.drop_duplicates(['id'])
 
     def clean_sold(sold_data: str) -> int:
@@ -136,10 +139,11 @@ def handle_initial_data(cfg):
     mean_shipping_cost = df['shippingCost'].astype(float).mean()
     df['shippingCost'] = df.fillna({'shippingCost': mean_shipping_cost}, inplace=True)
     df['sold'] = df['sold'].apply(clean_sold)
-    df.to_csv(cfg.db.sample_path, index=False)
+
+    return df
 
 
-def validate_initial_data(cfg):
+def validate_initial_data(cfg, sample):
     """This function declares expectations about the data features and validates them.
     Returns:
         result (bool): Status of data validation.
@@ -148,18 +152,15 @@ def validate_initial_data(cfg):
     # Create or open a data context
     try:
         context = gx.get_context(context_root_dir = "services/gx")
-    except Exception as e:
+    except Exception:
         context = FileDataContext(context_root_dir = "services/gx")
 
     # Add data source and data asset
     data_source = context.sources.add_or_update_pandas(name="sample")
-    data_asset = data_source.add_csv_asset(
-        name = "sample1",
-        filepath_or_buffer=cfg.db.sample_path
-    )
 
-    # Create batch request
-    batch_request = data_asset.build_batch_request()
+    data_asset = data_source.add_dataframe_asset(name = "pandas_dataframe")
+
+    batch_request = data_asset.build_batch_request(dataframe = sample)
 
     # Get an existing expectation suit
     context.get_expectation_suite("expectation_suite")
@@ -198,26 +199,6 @@ def validate_initial_data(cfg):
     # Open the data docs in a browser
     # context.open_data_docs()
     return checkpoint_result.success
-
-@hydra.main(version_base=None, config_path="../configs", config_name="main")
-def test_data(cfg: DictConfig = None):
-    """
-    This function creates a data sample and validates it.
-    """
-
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-    # take a sample
-    sample_data(cfg)
-    # validate the sample
-    try:
-        # if the validation failed, then try to handle the initial data
-        assert validate_initial_data(cfg)
-    except Exception as e:
-        handle_initial_data(cfg)
-    assert validate_initial_data(cfg)
-
-    print('Data is valid.')
 
 
 def read_datastore():
@@ -283,7 +264,7 @@ def validate_features(X,y):
     # Create or open a data context
     try:
         context = gx.get_context(context_root_dir = "services/gx")
-    except Exception as e:
+    except Exception:
         context = FileDataContext(context_root_dir = "services/gx")
 
     # Add data source and data asset
@@ -322,13 +303,6 @@ def validate_features(X,y):
 
     checkpoint_result = checkpoint.run()
 
-
-    # Build the data docs (website files)
-    # context.build_data_docs()
-
-    # Open the data docs in a browser
-    # context.open_data_docs()
-
     return checkpoint_result.success
 
 
@@ -338,6 +312,27 @@ def load_features(X,y,version):
     if zenml.load_artifact(version=version) is None:
         raise Exception("Artifact not loaded")
 
+
+@hydra.main(version_base=None, config_path="../configs", config_name="main")
+def test_data(cfg: DictConfig = None):
+    """
+    This function creates a data sample and validates it.
+    """
+
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+    # take a sample
+    sample = sample_data(cfg)
+    # validate the sample
+    try:
+        # if the validation failed, then try to handle the initial data
+        assert validate_initial_data(cfg, sample)
+    except Exception:
+        sample = handle_initial_data(cfg, sample)
+    
+    assert validate_initial_data(cfg, sample)
+
+    print('Data is valid.')
 
 if __name__ == '__main__':
     test_data()

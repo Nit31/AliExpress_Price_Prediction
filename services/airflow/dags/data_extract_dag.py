ab6446cd8a367b530ee158e2bbaae8aff2182504
Nit31 <dydalinnnov31@gmail.com>
2024-07-10 00:53:58 +0300

    Fix data version did not commit

--- services/airflow/dags/data_extract_dag.py
+++ services/airflow/dags/data_extract_dag.py
@@ -25,10 +25,19 @@ def version_data(cfg, sample):
     # Write the sample data to the file
     sample.to_csv(cfg.db.sample_path)
 
+    # Save data version
+    try:
+        with io.open(cfg.dvc.data_version_yaml_path, 'w', encoding='utf8') as outfile:
+            yaml.dump({"version" : cfg.db.sample_part}, outfile, default_flow_style=False, allow_unicode=True)
+    except Exception as e:
+        print(e)
+        raise
+
     # Use subprocess to run DVC commands
     try:
         subprocess.run(["dvc", "add", cfg.db.sample_path], check=True)
         subprocess.run(["git", "add", cfg.dvc.sample_info_path], check=True)
+        subprocess.run(["git", "add", cfg.dvc.data_version_yaml_path], check=True)
         # Commit the changes to Git repository.
         subprocess.run(["git", "commit", "-m", "Versioned sample data"], check=True)
         # Push the commit to Github.
@@ -39,14 +48,6 @@ def version_data(cfg, sample):
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while running shell commands: {e}")
         raise
-
-    # Save data version
-    try:
-        with io.open(cfg.dvc.data_version_yaml_path, 'w', encoding='utf8') as outfile:
-            yaml.dump({"version" : cfg.db.sample_part}, outfile, default_flow_style=False, allow_unicode=True)
-    except Exception as e:
-        print(e)
-        raise  
 
 
 def load_and_execute_sample_data(**kwargs):

7d7e58c6b1dab25f9d8d20e23752c467738a77d9
Nit31 <dydalinnnov31@gmail.com>
2024-07-20 14:37:07 +0300

    Update validate.py

--- src/validate.py
+++ src/validate.py
@@ -1,16 +1,36 @@
 import mlflow
+from mlflow import MlflowClient
+# from data import extract_data # custom module
+# from transform_data import transform_data # custom module
+# from model import retrieve_model_with_alias # custom module
 
 
-def validate(cfg=None):
-    registered_models = mlflow.registered_model.list_registered_models()
-    for i, model in enumerate(registered_models):
-        model_versions = mlflow.search_registered_model_versions(f"name='{model.name}'")
-        for version in model_versions:
-            mlflow.registered_model.add_alias(
-                name=model.name,
-                version=version.version,
-                alias=f'challenger{i}'
-            )
+def get_models_info_by_alias(alias):
+        models_with_alias = []
 
+        # Search for all registered models
+        for model_info in client.search_registered_models():
+            print(model_info.aliases)
+            model_aliases = model_info.aliases
+            if alias in model_aliases.keys():
+                models_with_alias.append(model_info)
 
+        return models_with_alias
+    
+    
+def main():
+    # Initialize the MLflow client
+    client = MlflowClient()
 
+    # Initialize hydra
+    hydra.initialize(config_path="../configs", job_name="preprocess_data", version_base=None)
+    cfg = hydra.compose(config_name="main")
+
+    # Get all models with the alias "Challenger"
+    models_with_challenger_alias = get_models_by_alias("challenger")
+    
+    
+
+    # Print all models with the alias
+    for model in models_with_challenger_alias:
+        print(f"Model Name: {model.name}, Alias: {model.aliases}")

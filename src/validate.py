cf8c33a5574455d10f8ae380aa3d650f96f1c12b
Nit31 <dydalinnnov31@gmail.com>
2024-07-23 18:55:34 +0300

    Loaded the model in validation

--- src/validate.py
+++ src/validate.py
@@ -1,24 +1,47 @@
 import mlflow
-from mlflow import MlflowClient
 import hydra
+from data import sample_data, validate_initial_data, handle_initial_data
+import giskard
 
 
 def get_models_info_by_alias(client, alias):
-        models_with_alias = []
+    models_with_alias = []
+    # Search for all registered models
+    for model_info in client.search_registered_models():
+        model_aliases = model_info.aliases
+        if alias in model_aliases.keys():
+            models_with_alias.append(model_info)
 
-        # Search for all registered models
-        for model_info in client.search_registered_models():
-            print(model_info.aliases)
-            model_aliases = model_info.aliases
-            if alias in model_aliases.keys():
-                models_with_alias.append(model_info)
+    return models_with_alias
 
-        return models_with_alias
+
+def extract_data(cfg, version):
+    # take a sample
+    sample = sample_data(cfg)
+    
+    # update the version of the sample
+    cfg.data_version.version = version
+
+    # validate the sample
+    try:
+        # if the validation failed, then try to handle the initial data
+        assert validate_initial_data(cfg, sample)
+    except Exception:
+        sample = handle_initial_data(sample)
+    assert validate_initial_data(cfg, sample)
+    
+    return sample
+
+
+def test_model(cfg, model_info, giscard_dataset):
+    # Load the model
+    model = mlflow.pytorch.load_model(model_info.latest_versions[0].source)
+    print(model)
     
     
 def main():
     # Initialize the MLflow client
-    client = MlflowClient()
+    client = mlflow.MlflowClient()
 
     # Initialize hydra
     hydra.initialize(config_path="../configs", job_name="preprocess_data", version_base=None)
@@ -27,8 +50,23 @@ def main():
     # Get all models with the alias "Challenger"
     models_with_challenger_alias = get_models_info_by_alias(client, "challenger")
     
+    # Wrap the raw dataset
+    version = cfg.mlflow.test_data_version
+    df = extract_data(cfg, version)
+    
+    TARGET_COLUMN = cfg.zenml.features.target
+    CATEGORICAL_COLUMNS = list(cfg.zenml.features.categorical)
+    
+    giskard_dataset = giskard.Dataset(
+        df=df,
+        target=TARGET_COLUMN,
+        cat_columns=CATEGORICAL_COLUMNS
+    )
+    
+    # Test all the challenger models
+    for model_info in models_with_challenger_alias:
+        test_model(cfg, model_info, giskard_dataset)
     
 
-    # Print all models with the alias
-    for model in models_with_challenger_alias:
-        print(f"Model Name: {model.name}, Alias: {model.aliases}")
+if __name__ == "__main__":
+    main()

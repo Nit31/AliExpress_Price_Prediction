1bfec850e90228547e8e2a712aaa001c389886bf
Amir <Amir-b04@mail.ru>
2024-07-24 13:13:44 +0300

    Update download_charts.py

--- src/download_charts.py
+++ src/download_charts.py
@@ -1,15 +1,27 @@
 import mlflow
 from mlflow.tracking import MlflowClient
 import os
+
 try:
     mlflow.set_tracking_uri("http://localhost:5000")
 except:
     pass
+
 # Initialize MLflow client
 client = MlflowClient()
 
-# Define the run ID and the artifact name
-run_id = "c77c9f07b6384284a2db164163a309d6"
+# Get the default experiment (usually with ID '0')
+experiment_name = "Default"
+experiment = client.get_experiment_by_name(experiment_name)
+
+# Get the latest run in the default experiment
+runs = client.search_runs(experiment_ids=[experiment.experiment_id], order_by=["start_time DESC"], max_results=1)
+
+if not runs:
+    raise Exception("No runs found in the default experiment.")
+
+latest_run = runs[0]
+run_id = latest_run.info.run_id
 artifact_name = "combined_metrics_plot.png"
 
 # Define the directory where you want to save the result

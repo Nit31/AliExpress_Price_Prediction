77ec868adf20cc3c56389909654f2e6d98155fed
Nit31 <dydalinnnov31@gmail.com>
2024-07-24 18:44:19 +0300

    Update evaluation

--- src/evaluate.py
+++ src/evaluate.py
@@ -16,16 +16,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def load_model_by_alias(model_name, alias):
-    client = MlflowClient()
+def get_models_info_by_alias(client, alias):
+    models_with_alias = []
+    # Search for all registered models
+    for model_info in client.search_registered_models(max_results=1000):
+        model_aliases = model_info.aliases
+        if any(alias in key for key in model_aliases.keys()):
+            models_with_alias.append(model_info)
 
-    # Get the model version associated with the alias
-    model_version = client.get_model_version_by_alias(model_name, alias).version
-
-    # Load the model
-    model = mlflow.pyfunc.load_model(f"models:/{model_name}/{model_version}")
-
-    return model
+    return models_with_alias
 
 
 def evaluate(data_sample_version, model_alias):
@@ -36,53 +35,38 @@ def evaluate(data_sample_version, model_alias):
     except Exception as e:
         print('Error loading zenml artifacts\n')
         raise e
-    client = MlflowClient()
-    registered_models = client.search_registered_models()
-    needed_model = None
-    for model in registered_models:
-        model_name = model.name
-
-
-        # Get all versions of the current model
-        versions = client.search_model_versions(f"name='{model_name}'")
-
-        # Iterate through each version
-        for version in versions:
-            version_number = version.version
-
-            # Retrieve the model version details
-            model_version = client.get_model_version(model_name, version_number)
-
-            # Get the aliases for this model version
-            aliases = model_version.aliases
-            # Download the model for each alias
-            for alias in aliases:
-                if alias == model_alias:
-                    needed_model = model
-                    break
-            if needed_model is not None:
-                break
-        if needed_model is not None:
-            break
+    
+    # Load the client
+    try:
+        mlflow.set_tracking_uri("http://localhost:5000")
+    except Exception:
+        pass
+    client = mlflow.MlflowClient()
+    
     X_test = data.drop(columns=['price'])
     y_test = data['price']
-    device = next(model.model.parameters()).device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     X_test_tensor = torch.tensor(X_test.values, dtype=torch.float32).to(device)
-    y_pred = needed_model(X_test_tensor).detach().cpu().numpy()
-    mae = mean_absolute_error(y_test, y_pred)
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
-    return mae, mse, r2
+    
+    models_with_alias = get_models_info_by_alias(client, model_alias)
+    for model_info in models_with_alias:
+        model = mlflow.pytorch.load_model(model_info.latest_versions[0].source, map_location=device)
+        with torch.no_grad():
+            y_pred = model(X_test_tensor).detach().cpu().numpy()
+        mae = mean_absolute_error(y_test, y_pred)
+        mse = mean_squared_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+        print(f'Model {model_info.name} with alias {model_alias} metrics are:')
+        print(f'MAE: {mae}, MSE: {mse}, R2: {r2}')
 
-@hydra.main(config_path="../configs", config_name='main', version_base=None)
-def main(cfg=None):
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_sample_version", type=str, default="sample_1", help="Data sample version")
-    parser.add_argument("--model_alias", type=str, default="champion", help="Model alias")
+    parser.add_argument("--version", type=str, default="sample_1", help="Data sample version")
+    parser.add_argument("--alias", type=str, default="champion", help="Model alias")
 
     args = parser.parse_args()
-    mae,mse,r2 = evaluate(args.data_sample_version, args.model_alias)
-    print(f'{mae},{mse},{r2}')
+    evaluate(args.version, args.alias)
+    
 
 
 if __name__ == "__main__":
